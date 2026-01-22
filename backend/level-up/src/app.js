578c9c89cefe1b9a4535d274010e0cede4de6ad3
Ryan-Mambou <ryanmambou@gmail.com>
2026-01-22 11:58:26 +0100

    chore: add swagger documentation

--- backend/level-up/src/app.js
+++ backend/level-up/src/app.js
@@ -5,10 +5,15 @@ const goalsRoutes = require('./routes/goals.routes')
 const habitsRoutes = require('./routes/habits.routes')
 const stepsRoutes = require('./routes/steps.routes')
 const logHabitsRoutes = require('./routes/habitLog.routes')
+const swaggerUi = require('swagger-ui-express')
+const swaggerSpec = require('./config/swagger')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
 app.use('/api/auth', authRoutes)
 app.use('/api/goals', goalsRoutes)
 app.use('/api/habits', habitsRoutes)
